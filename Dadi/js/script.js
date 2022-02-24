f3db057818ce191ff0f795b837dfcb273c1903e4
xVal4k <cristianbricicaru.cb@gmail.com>
2022-02-24 17:04:12 +0100

    js adjustment for the page

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,13 +1,17 @@
-let userNumber = Math.floor((Math.random() * 6) + 1);
-console.log(userNumber);
+let userNumber = document.querySelector(".user-number");
+let computerNumber = document.querySelector(".computer-number");
+let winner = document.querySelector(".winner");
+let resetButton = document.querySelector(".reset-button")
 
-let computerNumber = Math.floor((Math.random() * 6) + 1);
-console.log(computerNumber);
+resetButton.addEventListener("click", function() {
+    userNumber.innerHTML = Math.floor((Math.random() * 6) + 1);
+    computerNumber.innerHTML = Math.floor((Math.random() * 6) + 1);
 
-if (userNumber > computerNumber) {
-    console.log("The user won!");
-} else if (userNumber < computerNumber) {
-    console.log("The computer won!");
-} else if (userNumber == computerNumber) {
-    console.log("It's a draw!");
-}
+    if (userNumber.innerHTML > computerNumber.innerHTML) {
+        winner.innerHTML = "The user won!";
+    } else if (userNumber.innerHTML < computerNumber.innerHTML) {
+        winner.innerHTML ="The computer won!";
+    } else if (userNumber.innerHTML == computerNumber.innerHTML) {
+        winner.innerHTML ="It's a draw!";
+    }
+});
